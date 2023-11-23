4074efb161f5c60f02217399b14189d15baeb1f4
claras000 <clara.o.correa@web.de>
2023-11-23 19:20:25 +0100

    exercise3

--- clara-input/ChatGPT API/chatgbtChatAssistantWebside.py
+++ clara-input/ChatGPT API/chatgbtChatAssistantWebside.py
@@ -3,14 +3,17 @@ import gradio
 
 openai.api_key = "####"
 
-messages = [{"role": "system", "content": "You are a cinamavisitor that makes publicity for the next movie Starwars"}]
+messages = [{"role": "system", "content": "You are a cinamavisitor that makes publicity for the next movie called the Alita"}]
 
+
+#saving the chat and append it to message -> send it it gbt 
 def CustomChatGPT(user_input):
     messages.append({"role": "user", "content": user_input})
     response = openai.ChatCompletion.create(
         model = "gpt-3.5-turbo",
         messages = messages
     )
+    #safe reply
     ChatGPT_reply = response["choices"][0]["message"]["content"]
     messages.append({"role": "assistant", "content": ChatGPT_reply})
     return ChatGPT_reply
--- clara-input/ChatGPT API/chatgbtSimple.py
+++ clara-input/ChatGPT API/chatgbtSimple.py
@@ -2,5 +2,5 @@ import openai
 
 openai.api_key = "####"
 
-completion = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=[{"role": "user", "content": "Give me 3 ideas for apps I could build with openai apis "}])
+completion = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=[{"role": "user", "content": "please imitate a chat between customer and person who makes publicity for the movie called Alita"}])
 print(completion.choices[0].message.content)
